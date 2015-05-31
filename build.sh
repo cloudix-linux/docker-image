fbaa0c3859a7847ab9d574e0178a32c44b2874de
Robin <robin@naas.io>
2015-05-31 06:01:01 +0200

    Added dockerignore file. Added build wrapper

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+fs_file="cloudix-linux-fs.tar.bz2"
+
+if [ -f "$fs_file" ] ; then
+        checksum_prev=$(md5sum "$fs_file" |awk '{print $1}')
+fi
+
+./mkimage.sh sark/cloudix latest true
+
+checksum=$(md5sum "$fs_file" |awk '{print $1}')
+
+printf "\n"
+if [ -z "checksum_prev" ] ; then
+  echo "New $fs_file checksum is $checksum"
+else
+  echo "Previous $fs_file checksum was $checksum"
+  echo "New $fs_file checksum is $checksum"
+fi
+exit 0
